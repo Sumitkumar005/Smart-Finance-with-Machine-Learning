38eed35ae8f322c7132168927c260c2d7eab67dc
Sumit Kumar <sumitkumar969074@gmail.com>
2024-12-01 08:40:48 +0530

    Update deal_comparator.py

--- 5. ML Applications In Finance/22. Investment Banking/1. mergers_and_acquisitions_M&A/deal_comparator.py
+++ 5. ML Applications In Finance/22. Investment Banking/1. mergers_and_acquisitions_M&A/deal_comparator.py
@@ -1,4 +1,4 @@
-# Build a tool to compare past M&A deals based on various metrics like deal size, industry, and financial ratios,
+# Build a tool to compare past M&A deals based on various mettrics like deal size, industry, and financial ratios,
 # to gauge the attractiveness of a potential new deal.
 import requests
 import pandas as pd
